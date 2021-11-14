7bf4b2aa6734907d6bf187d6e2443062862d2a19
Akshar95 <patel.akshar295@gmail.com>
2021-11-14 10:02:27 +0000

    30 years debt

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,17 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  const num1 = parseInt(req.query.num1)
-  const num2 = parseInt(req.query.num2)
-  const total = num1 + num2
+  const currentSalary = parseInt(req.query.num1);
+  const monthlyThreshold = 2000;
+  const monthlySalary = currentSalary/12;
+  const aboveThreshold = monthlySalary-monthlyThreshold;
+  const interest = 0.09;
+  const monthlyInterest = aboveThreshold*interest;
+  const yearlyInterest = monthlyInterest*12;
+  const dueSum = yearlyInterest*30
 
-  res.send({total: total})
+
+  res.send({dueSum: dueSum})
 })
 
 // app.get('/:num1/:num2', function (req, res) {
