3bd8c9176c931795dc448d83119ac8ce9627a310
Akshar95 <patel.akshar295@gmail.com>
2021-12-17 10:42:03 +0000

    updated backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
 
 const yearlyThreshold = 27295;
 const monthlyThreshold = 2274;
@@ -8,47 +8,52 @@ const interest = 0.09;
 
 const calculateDueSum = (salary) => {
   const currentSalary = parseInt(salary);
-  const monthlySalary = currentSalary/12;
- //const actualIncome = currentSalary-tax(currentSalary);
-  const aboveMonthlyThreshold = monthlySalary-monthlyThreshold;
-  const monthlyInterest = aboveMonthlyThreshold*interest;
-  const yearlyInterest = monthlyInterest*12;
-  const dueSum = Math.round(yearlyInterest*30)
+  const monthlySalary = currentSalary / 12;
+  const aboveMonthlyThreshold = monthlySalary - monthlyThreshold;
+  const monthlyInterest = aboveMonthlyThreshold * interest;
+  const yearlyInterest = monthlyInterest * 12;
+  const dueSum = Math.round(yearlyInterest * 30);
 
-  return {dueSum, aboveMonthlyThreshold};
-} 
-
-// const debtCompound = () => {
-//   const currentDebt = parseInt(req.query.num2);
-  
-// }
-
-
-const tax = (currentSalary) => { 
-  const personalAllowance = 12570;
-  const taxableIncome= currentSalary - personalAllowance;
-    if (taxableIncome > 0 && taxableIncome <= 37700 ){
-      return  taxableIncome*0.2;
-    } else if (taxableIncome>37700){
-      return (37700*0.2) + ((taxableIncome-37700)*0.4)
-    }
-      return 0 
-  
+  return { dueSum, aboveMonthlyThreshold };
 };
 
-app.get('/calculate', function (req, res) {
-  try {
-    const {dueSum, aboveMonthlyThreshold}  = calculateDueSum(req.query.num1);
-    const calculatedTax = tax(req.query.num1);
-    // const debtCompound = debtCompound(req.query.num2);
-    res.json({dueSum: dueSum, tax: calculatedTax, aboveMonthlyThreshold})
-  } catch (err) {
-    console.error(err)
-    res.status(500)
+// adding this function to calculate compounded interest on debt
+const debtCompound = (currentDebt) => {
+  // const currentDebt = parseInt(req.query.num1);
+  const debtIn30Years = currentDebt * 30;
+
+  return debtIn30Years;
+};
+
+const tax = (currentSalary) => {
+  const personalAllowance = 12570;
+  const taxableIncome = currentSalary - personalAllowance;
+  if (taxableIncome > 0 && taxableIncome <= 37700) {
+    return taxableIncome * 0.2;
+  } else if (taxableIncome > 37700) {
+    return 37700 * 0.2 + (taxableIncome - 37700) * 0.4;
   }
-})
+  return 0;
+};
 
+const akshar =  (req, res) => {
+  try {
+    
+    const { dueSum, aboveMonthlyThreshold } = calculateDueSum(req.query.num1);
+    const calculatedTax = tax(req.query.num1);
+    const debtIn30Years = debtCompound(req.query.num1);
+    res.json({
+      dueSum: dueSum,
+      tax: calculatedTax,
+      aboveMonthlyThreshold: aboveMonthlyThreshold,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+  }
+}
 
+app.get("/calculate", akshar);
 
 // app.get('/:num1/:num2', function (req, res) {
 //   const num1 = parseInt(req.params.num1)
@@ -59,13 +64,17 @@ app.get('/calculate', function (req, res) {
 // dev/?num1=51&num2=69
 // dev/51/69
 
-module.exports = {handler : serverless(app), tax: tax, calculateDueSum: calculateDueSum};
+module.exports = {
+  handler: serverless(app),
+  tax: tax,
+  calculateDueSum: calculateDueSum,
+  debtCompound: debtCompound,
+};
 
 /***
  * query parameter
  * json body in api
  * search request headers/api headers
- * serverless offline 
- * 
+ * serverless offline
+ *
  */
-
--- frontend/studentloan/components/Calculate.js
+++ frontend/studentloan/components/Calculate.js
@@ -1,49 +1,58 @@
-import React from 'react'
-import {useState} from 'react'
-import styles from './Calculate.module.css'
+import React from "react";
+import { useState } from "react";
+import styles from "./Calculate.module.css";
 
 export const Form = () => {
-    const [dueSum, setDueSum]= useState(0)
-    const [aboveMonthlyThreshold, setaboveMonthlyThreshold] = useState(0)
-    // const [debtCompound, setdebtCompound] = useState(0)
-    const calculateSum = async event => {
-        event.preventDefault()
-        
-        const res = await fetch(
-            `http://localhost:3000/calculate/?num1=${event.target.currentSalary.value}&num2=${event.target.currentDebt.value}`)
-        
-            const result = await res.json();
-            
-            setDueSum(result.dueSum);  
-            setaboveMonthlyThreshold(result.aboveMonthlyThreshold);
-            // setdebtCompound(result.debtCompound);
-              
-             
-    }
+  const [dueSum, setDueSum] = useState(0);
+  //const [aboveMonthlyThreshold, setaboveMonthlyThreshold] = useState(0);
+  // const [debtCompound, setdebtCompound] = useState(0)
+  const calculateSum = async (event) => {
+    event.preventDefault();
 
-    return (
+    const res = await fetch(
+      `http://localhost:3000/calculate/?num1=${event.target.currentSalary.value}&num2=${event.target.currentDebt.value}`
+    );
+
+    const result = await res.json();
+    console.log(result);
+    setDueSum(result.dueSum);
+    //setaboveMonthlyThreshold(result.aboveMonthlyThreshold);
+    // setdebtCompound(result.debtCompound);
+  };
+
+  return (
     <>
-        <form onSubmit={calculateSum}>
-            <ul className={styles.list}>
-                <li><label htmlFor="currentSalary">Current Salary: </label>
-                <input type="text" placeholder="30000" name="currentSalary" id="currentSalary" /> </li>
-                
-                <li><label htmlFor="currentDebt">Current Debt: </label>
-                <input type="text" placeholder="50000" name="currentDebt" id="currentDebt" /> </li>
+      <form onSubmit={calculateSum}>
+        <ul className={styles.list}>
+          <li>
+            <label htmlFor="currentSalary">Current Salary: </label>
+            <input
+              type="text"
+              placeholder="30000"
+              name="currentSalary"
+              id="currentSalary"
+            />{" "}
+          </li>
 
-                <button className={styles.button}type='submit'>Calculate</button> 
+          <li>
+            <label htmlFor="currentDebt">Current Debt: </label>
+            <input
+              type="text"
+              placeholder="50000"
+              name="currentDebt"
+              id="currentDebt"
+            />{" "}
+          </li>
 
-            </ul>
-        </form>
-            
-        <h1>Amount due over next 30 years: {dueSum}</h1>
-        <h1>Above monthly Threshold {aboveMonthlyThreshold}</h1>
-        {/* <h1>Debt Amount{debtCompound}</h1> */}
-        
-        </>
-    )
-}
+          <button className={styles.button} type="submit">
+            Calculate
+          </button>
+        </ul>
+      </form>
 
-
-
- 
+      <h1>Amount due over next 30 years: {dueSum}</h1>
+      {/* <h1>Above monthly Threshold {aboveMonthlyThreshold}</h1> */}
+      {/* <h1>Debt Amount{debtCompound}</h1> */}
+    </>
+  );
+};
